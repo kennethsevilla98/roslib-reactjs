7ce620cc5df5ad45915f99737ea972fa9b079ade
ltiseni94 <luca13tiseni@gmail.com>
2021-12-29 10:27:32 +0100

    added license, readme and entry point

---
+++ README.md
@@ -0,0 +1,6 @@
+# The roslib-reactjs package
+
+A collection of context providers for creating GUI with ReactJS and React Native
+
+clone and install using npm!
+
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import DeleteParam from "./DeleteParam";
+import { GetParam, useParam } from "./GetParam";
+import SetParam from "./SetParam";
+import { ParamListProvider, useParamList } from "./ParamList";
+import Publisher from "./Publisher";
+import { Subscriber, useMsg } from "./Subscriber";
+import { TopicListProvider, useTopicList } from "./TopicList";
+import ServiceCaller from "./ServiceCaller";
+import ServiceServer from "./ServiceServer";
+import { ServiceListProvider, useServiceList } from "./ServiceList";
+import RosConnect from "./RosConnect";
+
