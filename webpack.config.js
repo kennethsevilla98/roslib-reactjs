6310cea7d9cb1b16eeb27886711862f8230ce2b7
ltiseni94 <luca13tiseni@gmail.com>
2021-12-29 14:59:03 +0100

    trying commonjs2

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.js',
-        libraryTarget: 'umd',
+        libraryTarget: 'commonjs2',
     },
     module: {
         rules: [
